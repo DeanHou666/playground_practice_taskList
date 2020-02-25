767e8d7b5b110333ffc6f27da6988192373f7340
侯本涛 <houbentao@MacBook-Pro.local>
2020-02-25 18:06:48 +0800

    number guess is done

--- app.js
+++ app.js
@@ -1,9 +1,65 @@
-const loanAmount = document.querySelector('#amount');
-const loanInterest = document.querySelector('#interest')
+const guess = document.querySelector('#guessInput')
+const form = document.querySelector('#guess-form')
+const output=document.querySelector('.output')
+const submitBtn = document.querySelector('#submit')
+const messageInfo = document.querySelector('.output')
 
 
-function c (){
+let maxNumber=10
+ minNumber=1
+ guessLeft=3;
+ rightGuess = 3
+
+
+submitBtn.addEventListener('click',guessStart);
+form.addEventListener('mousedown',restartGame)
+
+
+function restartGame(e){
+  if(e.target.classList.contains('play-again')){
+    window.location.reload()
+  }
+}
+
+function guessStart(e){
+  e.preventDefault()
+  let guessValue = parseInt(guess.value);
+  if( isNaN(guessValue) || guessValue<minNumber || guessValue>maxNumber){
+    showMessage('check input','alert alert-danger');
+  }else{
+    if( guessValue != rightGuess){
+      guessLeft = guessLeft-1;
+      showMessage(`wrong, ${guessLeft} is left`,'alert alert-warning')
+      guess.value=''
+      if(guessLeft === 0){
+        gameOver();
+      }
+    }else{
+      showMessage('you win,good boy','alert alert-success')
+      gameOver();
+    }
+  }
+}
+
+function gameOver(){
+  guess.disabled=true;
+  submitBtn.value='play again?'
+  submitBtn.className='play-again'
 }
 
 
-document.querySelector('#load-form').addEventListener('submit',c)
+function showMessage(msg,className){
+  clearMessage()
+  messageInfo.appendChild(document.createTextNode(msg))
+  messageInfo.className=className
+  setTimeout(() => {
+    clearMessage()
+  }, 2000);
+}
+
+function clearMessage(){
+  if(messageInfo.firstChild){
+    messageInfo.textContent=''
+    messageInfo.className=''
+  }
+}
